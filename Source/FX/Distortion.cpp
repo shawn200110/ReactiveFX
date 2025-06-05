6ee919da6eaa536ae21a667528639402ebda1bae
shawn200110 <shawncawley2125@gmail.com>
2025-06-05 10:49:02 -0400

    Populated Distortion DSP

--- Source/FX/Distortion.cpp
+++ Source/FX/Distortion.cpp
@@ -1,10 +1,48 @@
 #include "Distortion.h"
 
-
-void Distortion::prepareToPlay(double sampleRate, int samplesPerBlock) {
+void Distortion::prepare(const juce::dsp::ProcessSpec& spec) {
 };
-void Distortion::processBlock(juce::AudioBuffer<float>& buffer) {
+
+void Distortion::processBlock(juce::AudioBuffer<float>& buffer,
+                              const int numInputChannels,
+                              const int numOutputChannels) {
+    juce::ScopedNoDenormals noDenormals;
+
+    // In case we have more outputs than inputs, this code clears any output
+    // channels that didn't contain input data, (because these aren't
+    // guaranteed to be empty - they may contain garbage).
+    // This is here to avoid people getting screaming feedback
+    // when they first compile a plugin, but obviously you don't need to keep
+    // this code if your algorithm always overwrites all the output channels.
+    for (auto i = numInputChannels; i < numOutputChannels; ++i)
+        buffer.clear(i, 0, buffer.getNumSamples());
+
+    // THESE ARE HARDCODED, NEED TO CONNECT TO PARAMETER
+    float drive = 1;
+    float range = 1;
+    float blend = 1;
+    float volume = 1;
+
+
+    // Make sure to reset the state if your inner loop is processing
+    // the samples and the outer loop is handling the channels.
+    // Alternatively, you can process the samples with the channels
+    // interleaved by keeping the same state.
+    for (int channel = 0; channel < numInputChannels; ++channel)
+    {
+        auto* channelData = buffer.getWritePointer(channel);
+
+        for (int sample = 0; sample < buffer.getNumSamples(); sample++) {
+
+            float cleanData = *channelData;
+
+            *channelData *= drive * range;
+
+            *channelData = (((((2.f / juce::float_Pi) * atan(*channelData)) * blend) + (cleanData * (1.f - blend))) / 2.f) * volume;
+
+            channelData++;
+        }
+    }
 };
 void Distortion::setParameters(int test) {
 };
-
--- Source/FX/Distortion.h
+++ Source/FX/Distortion.h
@@ -1,9 +1,14 @@
 #include <JuceHeader.h>
 class Distortion {
 public:
-    void prepareToPlay(double sampleRate, int samplesPerBlock);
-    void processBlock(juce::AudioBuffer<float>& buffer);
+    void prepare(const juce::dsp::ProcessSpec& spec);
+    void processBlock(juce::AudioBuffer<float>& buffer,
+                      const int numInputChannels,
+                      const int numOutputChannels);
     void setParameters(int test);
+
 private:
-    // Internal state and processing variables
+
+    juce::AudioProcessorValueTreeState state;
+
 };
