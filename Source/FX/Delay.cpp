2d5c8b994c161639edc2671d8b338e573bf69958
shawn200110 <shawncawley2125@gmail.com>
2025-06-05 10:31:28 -0400

    Populated Delay DSP

--- Source/FX/Delay.cpp
+++ Source/FX/Delay.cpp
@@ -1,10 +1,64 @@
 #include "Delay.h"
 
 
-void Delay::prepareToPlay(double sampleRate, int samplesPerBlock) {
+void Delay::prepare(const juce::dsp::ProcessSpec& spec) {
+    // Use this method as the place to do any pre-playback
+    // initialisation that you need..
+    delayBufferLength = (int)2.0 * spec.sampleRate;
+    if (delayBufferLength < 1)
+        delayBufferLength = 1;
+
+    delayBuffer.setSize(2, delayBufferLength);
+    delayBuffer.clear();
+
+    delayReadPosition = (int)(delayWritePosition - (delayLength * spec.sampleRate) + delayBufferLength) % delayBufferLength;
 };
-void Delay::processBlock(juce::AudioBuffer<float>& buffer) {
+void Delay::processBlock(juce::AudioBuffer<float>& buffer, 
+                         const int numInputChannels,
+                         const int numOutputChannels) {
+    const int numSamples = buffer.getNumSamples();
+
+    int dpr, dpw;
+
+    // This is the place where you'd normally do the guts of your plugin's
+    // audio processing...
+    for (int channel = 0; channel < numInputChannels; ++channel) {
+        float* channelData = buffer.getWritePointer(channel);
+        float* delayData = delayBuffer.getWritePointer(juce::jmin(channel, delayBuffer.getNumChannels() - 1));
+
+        dpr = delayReadPosition;
+        dpw = delayWritePosition;
+
+        for (int i = 0; i < numSamples; ++i) {
+            const float in = channelData[i];
+            float out = 0.0;
+
+            out = (dryMix * in + wetMix * delayData[dpr]);
+
+            delayData[dpw] = in + (delayData[dpr] * feedback);
+
+            if (++dpr >= delayBufferLength)
+                dpr = 0;
+            if (++dpw >= delayBufferLength)
+                dpw = 0;
+
+            channelData[i] = out;
+        }
+    }
+
+    delayReadPosition = dpr;
+    delayWritePosition = dpw;
+
+    // In case we have more outputs than inputs, this code clears any output
+    // channels that didn't contain input data, (because these aren't
+    // guaranteed to be empty - they may contain garbage).
+    // I've added this to avoid people getting screaming feedback
+    // when they first compile the plugin, but obviously you don't need to
+    // this code if your algorithm already fills all the output channels.
+    for (int i = numInputChannels; i < numOutputChannels; ++i)
+        buffer.clear(i, 0, buffer.getNumSamples());
 };
+
 void Delay::setParameters(int test) {
 };
 
--- Source/FX/Delay.h
+++ Source/FX/Delay.h
@@ -1,9 +1,31 @@
 #include <JuceHeader.h>
 class Delay {
 public:
-    void prepareToPlay(double sampleRate, int samplesPerBlock);
-    void processBlock(juce::AudioBuffer<float>& buffer);
+    void prepare(const juce::dsp::ProcessSpec& spec);
+    void processBlock(juce::AudioBuffer<float>& buffer,
+                      const int numInputChannels,
+                      const int numOutputChannels);
     void setParameters(int test);
+    int lastUIWidth, lastUIHeight;
+
+    enum Parameters
+    {
+        delayLengthParam = 0,
+        dryMixParam,
+        wetMixParam,
+        feedbackParam,
+        numParameters
+    };
+
+    float delayLength;
+    float dryMix;
+    float wetMix;
+    float feedback;
+
 private:
-    // Internal state and processing variables
+    // circular buffer variables
+    juce::AudioSampleBuffer delayBuffer;
+    int delayBufferLength;
+    int delayReadPosition;
+    int delayWritePosition;
 };
